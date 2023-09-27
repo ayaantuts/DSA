7c6129745b6fcff821b20656b2bd7040402b72d6
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-09-27 19:41:31 +0530

    Fixed Linked List program [0008.c](./0008.c)

--- 0008.c
+++ 0008.c
@@ -8,92 +8,123 @@ struct Node {
 	struct Node* next;
 } *start;
 
-void insert(int data) {
-	if (!start) {
-		start = (struct Node*) malloc(sizeof(struct Node));
-		start->data = data;
-		start->next = NULL;
-		return;
-	}
-	struct Node* temp = start;
-	while (temp->next != NULL) {
-		temp = temp->next;
-	}
-	temp->next = (struct Node*) malloc(sizeof(struct Node));
-	temp->next->data = data;
-	temp->next->next = NULL;
+void insert_begin(int val) {
+	struct Node* new_node;
+	new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->data = val;
+	new_node->next = start;
+	start = new_node;
 }
 
-void delete(int data) {
-	if (!start) {
-		printf("\nList is empty!");
-		return;
+void insert_before(int val, int num) {
+	struct Node* new_node, *ptr;
+	new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->data = val;
+	ptr = start;
+	if (ptr->data == num) {
+		start = new_node;
+		new_node->next = ptr;
 	}
-	struct Node* del;
-	if (start->data == data) {
-		if (start->next == NULL) {
-			free(start);
-			start = NULL;
-			return;
+	else {
+		while (ptr->next->data != num) {
+			ptr = ptr->next;
 		}
-		del = start;
-		start = start->next;
-		free(del);
-		return;
+		new_node->next = ptr->next;
+		ptr->next = new_node;
 	}
-	struct Node* temp = start;
-	while(temp->next != NULL && temp->next->data != data) {
-		temp = temp->next;
-	}
-	if (temp->next == NULL) {
-		printf("\nElement not found!");
-		return;
-	}
-	del = temp->next;
-	temp->next = temp->next->next;
-	free(del);
 }
 
-void display() {
+void insert_after(int val, int num) {
+	struct Node* new_node, *ptr;
+	new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->data = val;
+	ptr = start;
+	while (ptr->data != num) {
+		ptr = ptr->next;
+	}
+	new_node->next = ptr->next;
+	ptr->next = new_node;
+}
+
+void delete_node(int val) {
 	if (!start) {
-		printf("\nList is empty!");
+		printf("The list is empty\n");
+	}
+	else {
+		struct Node* ptr, *preptr;
+		ptr = start;
+		if (ptr->data == val) {
+			start = ptr->next;
+			free(ptr);
+		} else {
+			while (ptr->data != val) {
+				preptr = ptr;
+				ptr = ptr->next;
+			}
+			preptr->next = ptr->next;
+			free(ptr);
+		}
+	}
+}
+
+void print() {
+	struct Node* ptr;
+	if (!start) {
+		printf("The list is empty\n");
 		return;
 	}
-	struct Node* temp = start;
-	printf("\n");
-	while(temp->next != NULL) {
-		printf("%d->", temp->data);
-		temp = temp->next;
+	else {
+		ptr = start;
+		printf("The list is: ");
+		while (ptr->next != NULL) {
+			printf("%d->", ptr->data);
+			ptr = ptr->next;
+		}
+		printf("%d", ptr->data);
+		printf("\n");
 	}
-	printf("%d\n", temp->data);
 }
 
 int main() {
 	start = NULL;
 	int choice, data;
 	do {
-		printf("\n1. Insert\n2. Delete\n3. Print\n4. Exit\nEnter a choice: ");
+		printf("\n1. Insert Begin\n2. Insert Before\n3. Insert after\n4. Delete a node\n5. Print\n6. Exit\n\nEnter a choice: ");
 		scanf("%d", &choice);
-		switch (choice) {
+		switch(choice) {
 			case 1:
-				printf("\nEnter the value: ");
+				printf("Enter data: ");
 				scanf("%d", &data);
-				insert(data);			
+				insert_begin(data);
 				break;
 			case 2:
-				printf("\nEnter the data to delete: ");
+				printf("Enter data: ");
 				scanf("%d", &data);
-				delete(data);
+				printf("Enter number before which you want to insert: ");
+				int num;
+				scanf("%d", &num);
+				insert_before(data, num);
 				break;
 			case 3:
-				display();
+				printf("Enter data: ");
+				scanf("%d", &data);
+				printf("Enter number after which you want to insert: ");
+				scanf("%d", &num);
+				insert_after(data, num);
 				break;
 			case 4:
+				printf("Enter data: ");
+				scanf("%d", &data);
+				delete_node(data);
+				break;
+			case 5:
+				print();
+				break;
+			case 6:
 				break;
 			default:
-				printf("\nInvalid choice!");
-				choice = 4;
+				printf("Invalid choice\n");
 		}
-	} while (choice != 4);
+	} while (choice != 6);
 	return 0;
 }
