89ea02670917f3d8d40e54a1a64e88ea97ec54a3
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-11-19 16:22:59 +0530

    Fix sorting algorithm in 0025.c

--- 0025.c
+++ 0025.c
@@ -3,7 +3,7 @@
 #include <conio.h>
 
 int main() {
-	int i, j, n, arr[100], min, temp, swap;
+	int i, j, n, arr[100], min, temp;
 	printf("Enter the number of elements: ");
 	scanf("%d", &n);
 	printf("Enter the elements: ");
@@ -11,13 +11,19 @@ int main() {
 		scanf("%d", &arr[i]);
 	}
 
-	for (i = 0; i < n - 1; i++)
-		for (j = i + 1; j < n; j++)
-			if (arr[j] < arr[i]) {
-				temp = arr[j];
-				arr[j] = arr[i];
-				arr[i] = temp;
+	for (i = 0; i < n - 1; i++) {
+		min = i;
+		for (j = i; j < n; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
 			}
+		}
+		if (min != i) {
+			temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}	
+	}
 
 	printf("\nSorted array: ");
 	for (i = 0; i < n; i++) {
