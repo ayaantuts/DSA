5140e8b9fccb8ca8bd33a027e097d23297615c73
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-11-23 20:19:23 +0530

    Update

--- 0024.c
+++ 0024.c
@@ -12,7 +12,7 @@ int main() {
 	}
 	for (i = 0; i < n - 1; i++) {
 		for (j = i + 1; j < n; j++) {
-			if (arr[j] < arr[i]) {
+			if (arr[i] > arr[j]) {
 				temp = arr[j];
 				arr[j] = arr[i];
 				arr[i] = temp;
