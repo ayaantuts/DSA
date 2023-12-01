18bba926205f955e5e0dc96a832a2830c51236ab
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-12-01 20:54:40 +0530

    Refactor polynomial addition and subtraction

--- 0010.c
+++ 0010.c
@@ -29,75 +29,100 @@ struct Node* createLL(struct Node* start) {
 	return start;
 }
 
-struct Node* insertEnd(struct Node* start, int c, int p) {
-	struct Node *newnode, *temp = start;
+// struct Node* insertEnd(struct Node* start, int c, int p) {
+// 	struct Node *newnode, *temp = start;
+// 	newnode = (struct Node*) malloc(sizeof(struct Node));
+// 	newnode->coeff = c;
+// 	newnode->pow = p;
+// 	if (!start)
+// 		start = newnode;
+// 	else {
+// 		while (temp->next != NULL)
+// 			temp = temp->next;
+// 		temp->next = newnode;
+// 	}
+// 	newnode->next = NULL;
+// 	return start;
+// }
+
+struct Node* createNode(int c, int p) {
+	struct Node *newnode;
 	newnode = (struct Node*) malloc(sizeof(struct Node));
 	newnode->coeff = c;
 	newnode->pow = p;
-	if (!start)
-		start = newnode;
-	else {
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = newnode;
-	}
 	newnode->next = NULL;
-	return start;
+	return newnode;
 }
 
 void addIt() {
-	struct Node *t1 = start1, *t2 = start2;
+	int c, p;
+	struct Node *t1 = start1, *t2 = start2, *t3 = start3;
 	while(t1 && t2) {
 		if (t1->pow > t2->pow) {
-			start3 = insertEnd(start3, t1->coeff, t1->pow);
+			c = t1->coeff;
+			p = t1->pow;
 			t1 = t1->next;
 		}
 		else if (t1->pow < t2->pow) {
-			start3 = insertEnd(start3, t2->coeff, t2->pow);
+			c = t2->coeff;
+			p = t2->pow;
 			t2 = t2->next;
 		}
 		else {
-			start3 = insertEnd(start3, t1->coeff + t2->coeff, t1->pow);
+			c = t1->coeff + t2->coeff;
+			p = t1->pow;
 			t1 = t1->next;
 			t2 = t2->next;
 		}
+		t3->next = createNode(c, p);
+		t3 = t3->next;
 	}
 	while (t1) {
-		start3 = insertEnd(start3, t1->coeff, t1->pow);
+		t3->next = createNode(t1->coeff, t1->pow);
 		t1 = t1->next;
+		t3 = t3->next;
 	}
 	
 	while (t2) {
-		start3 = insertEnd(start3, t2->coeff, t2->pow);
+		t3->next = createNode(t2->coeff, t2->pow);
 		t2 = t2->next;
+		t3 = t3->next;
 	}
 }
 
 void subIt() {
-	struct Node *t1 = start1, *t2 = start2;
+	int c, p;
+	struct Node *t1 = start1, *t2 = start2, *t4 = start4;
 	while(t1 && t2) {
 		if (t1->pow > t2->pow) {
-			start4 = insertEnd(start4, t1->coeff, t1->pow);
+			c = t1->coeff;
+			p = t1->pow;
 			t1 = t1->next;
 		}
 		else if (t1->pow < t2->pow) {
-			start4 = insertEnd(start4, -1 * t2->coeff, t2->pow);
+			c = -1 * t2->coeff;
+			p = t2->pow;
 			t2 = t2->next;
 		}
 		else {
-			start4 = insertEnd(start4, t1->coeff - t2->coeff, t1->pow);
+			c = t1->coeff - t2->coeff;
+			p = t1->pow;
 			t1 = t1->next;
 			t2 = t2->next;
 		}
+		t4->next = createNode(c, p);
+		t4 = t4->next;
 	}
 	while (t1) {
-		start4 = insertEnd(start4, t1->coeff, t1->pow);
+		t4->next = createNode(t1->coeff, t1->pow);
 		t1 = t1->next;
+		t4 = t4->next;
 	}
 	
 	while (t2) {
-		start4 = insertEnd(start4, -1 * t2->coeff, t2->pow);
+		t4->next = createNode(-1 * t2->coeff, t2->pow);
 		t2 = t2->next;
+		t4 = t4->next;
 	}
 }
 
@@ -113,13 +138,29 @@ void display(struct Node* start) {
 	printf("\n");
 }
 
+// Remove dummy nodes
+void removeDummy() {
+	struct Node *temp = start3;
+	start3 = start3->next;
+	free(temp);
+	temp = start4;
+	start4 = start4->next;
+	free(temp);
+}
+
 int main() {
-	start1 = start2 = start3 = start4 =  NULL;
+	start1 = start2 = NULL;
 	start1 = createLL(start1);
 	start2 = createLL(start2);
+	// Dummy nodes
+	start3 = createNode(0, 0);
+	start4 = createNode(0, 0);
 	addIt();
 	subIt();
+	removeDummy();
+	printf("\nPolynomial 1: ");
 	display(start1);
+	printf("\nPolynomial 2: ");
 	display(start2);
 	printf("\nAddition: ");
 	display(start3);
