1e5e5fb1e5b9174ed7a7a1476e2b1010fee6b516
Ayaan Shaikh <ayaanmaths04@gmail.com>
2024-01-06 18:41:55 +0530

    Add user input for adjacency matrix

--- 0019.c
+++ 0019.c
@@ -5,6 +5,8 @@
 int visited[NODES] = { 0 };
 int visitedCount = 0;
 char nodes[NODES] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
+int adj[NODES][NODES] = { 0 };
+/*
 int adj[NODES][NODES] = {
 //   A  B  C  D  E  F  G  H 
 	{0, 1, 1, 1, 0, 0, 0, 0},
@@ -16,6 +18,7 @@ int adj[NODES][NODES] = {
 	{0, 0, 0, 0, 0, 0, 0, 0},
 	{0, 0, 0, 0, 0, 1, 1, 0}
 };
+*/
 int decOrder[NODES] = { 0 };
 int incoming[NODES] = { 0 };
 
@@ -49,6 +52,11 @@ void topologicalSort() {
 
 int main() {
 	int i, j;
+	printf("Enter the adjacency matrix of %c: ", nodes[i]);
+	for (i = 0; i < NODES; i++)
+		for (j = 0; j < NODES; j++)
+			scanf("%d", &adj[i][j]);
+
 	calcIncoming();
 	topologicalSort();
 	return 0;
